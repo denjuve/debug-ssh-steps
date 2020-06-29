d3c53273076a855848ff2c90b6b593feb5c572d8
den <dkucherenko@mirantis.com>
2020-06-29 20:07:00 +0300

    2remote+ lib

---
+++ src/com/libs/lib1.groovy
@@ -0,0 +1,19 @@
+package com.libs
+
+def func(r_ip)
+
+def remote = [:]
+remote.name = "${r_ip}"
+remote.host = "${r_ip}"
+remote.allowAnyHosts = true
+
+  withCredentials([sshUserPrivateKey(credentialsId: ssh_creds, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
+      usernamePassword(credentialsId: '5gt-ci', usernameVariable: 'u5g', passwordVariable: 'p5g')]) {
+        remote.user = userName
+        remote.identityFile = identity
+
+
+sshCommand remote: remote1, command: "ls -l"
+
+}}
+return this
